38f58e246dba95c6ca07aae5d6c35a5ca8ba31cc
BillyDGold <74416226+BillyDGold@users.noreply.github.com>
2020-11-25 10:41:36 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+var typed = new Typed(".typing", {
+    strings: ["Coder", "Musician", "Photographer", "Editor"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
